9b7702222d0bbaf1ba3f3b9ee1802adc82cd2f0a
cexbrayat <cedric@ninja-squad.com>
2020-03-31 00:13:10 +0000

    chore: version 7.3.10

---
+++ ponyracer/projects/ponyracer-lib/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ponyracer-lib
+ */
+
+export * from './lib/ponyracer-lib.service';
+export * from './lib/ponyracer-lib.component';
+export * from './lib/ponyracer-lib.module';
