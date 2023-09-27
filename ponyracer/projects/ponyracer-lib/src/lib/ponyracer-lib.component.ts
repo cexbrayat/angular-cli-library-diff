34702b9feae536eac632dc8206ba22d8a4b09fc1
cexbrayat <cedric@ninja-squad.com>
2023-09-27 12:27:29 +0000

    chore: version 17.0.0-next.6

--- ponyracer/projects/ponyracer-lib/src/lib/ponyracer-lib.component.ts
+++ ponyracer/projects/ponyracer-lib/src/lib/ponyracer-lib.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'lib-ponyracer-lib',
+  standalone: true,
+  imports: [CommonModule],
   template: `
     <p>
       ponyracer-lib works!
