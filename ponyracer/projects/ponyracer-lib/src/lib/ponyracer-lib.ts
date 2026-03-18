f651c9d9bdf94215ed1e8b5e0f7e03c8bb68f1f9
cexbrayat <cedric@ninja-squad.com>
2026-03-18 20:38:31 +0000

    chore: version 22.0.0-next.2

--- ponyracer/projects/ponyracer-lib/src/lib/ponyracer-lib.ts
+++ ponyracer/projects/ponyracer-lib/src/lib/ponyracer-lib.ts
@@ -1,10 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'lib-ponyracer-lib',
   imports: [],
   template: ` <p>ponyracer-lib works!</p> `,
   styles: ``,
-  changeDetection: ChangeDetectionStrategy.Eager,
 })
 export class PonyracerLib {}
